a75095b5969992fe3fea0bbf042ea4538eb9ac7a
GalloJunior <gallojunior@gmail>
2023-10-25 11:06:25 -0300

    Finalização da camada de conexão de dados

--- CozaStore/Models/Categoria.cs
+++ CozaStore/Models/Categoria.cs
@@ -28,6 +28,7 @@ public class Categoria
     [ForeignKey("CategoriaPaiId")]
     public Categoria CategoriaPai { get; set; }
     
+    public ICollection<ProdutoCategoria> Produtos { get; set; }
 }
 
 
--- CozaStore/Models/Usuario.cs
+++ CozaStore/Models/Usuario.cs
@@ -24,4 +24,6 @@ public class Usuario
     [StringLength(300)]
     public string Foto { get; set; }
 
+    public ICollection<ProdutoAvaliacao> Avaliacoes { get; set; }
+    public ICollection<ListaDesejo> ListaDesejos { get; set; }
 }
